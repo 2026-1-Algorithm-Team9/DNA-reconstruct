6b3d94c48d968649c82a87a9ce8a49a34e870991
Jaeyeon Lee <leejaeyeon2877@gmail.com>
2026-05-27 15:52:00 +0900

    조각 염기 서열 함수 수정

--- makeRef.c
+++ makeRef.c
@@ -11,7 +11,6 @@ char* makeRef() {
 
     // 염기 서열 저장하기 위한 배열 동적 할당 (+1은 문자열 종료 문자 '\0'를 위해)
     char* ref = (char*)malloc((refLength + 1) * sizeof(char));
-    srand(time(NULL));
 
     for (int i = 0; i < refLength; i++) {
         int index = rand() % 4;
@@ -22,29 +21,36 @@ char* makeRef() {
     return ref;
 }
 
-char** makeFrag(char* ref) {
-    char** frags = (char**)malloc(fragNum * sizeof(char*));     // 조각들을 저장하기 위한 2차원 배열을 동적으로 할당
+char** makeFrag() {
+    // A, T, C, G 염기를 배열로 정의
+    char bases[4] = {'A', 'T', 'C', 'G'};
+    
+    // 조각들을 저장하기 위한 2차원 배열 동적 할당
+    char** frags = (char**)malloc(fragNum * sizeof(char*)); 
 
     for (int i = 0; i < fragNum; i++) {
-        frags[i] = (char*)malloc((fragLength+1) * sizeof(char));     // 각 조각을 저장하기 위한 배열 동적 할당 (+1은 문자열 종료 문자 '\0'를 위해)
+        // 각 조각을 저장하기 위한 배열 동적 할당 (+1은 '\0' 자리를 위함)
+        frags[i] = (char*)malloc((fragLength + 1) * sizeof(char)); 
 
-        int startIndex = rand() % (refLength - fragLength + 1);     // 원본 염기서열에서 조각을 시작할 랜덤한 인덱스 생성
         for (int j = 0; j < fragLength; j++) {
-            frags[i][j] = ref[startIndex + j];     // 원본 염기서열에서 조각을 추출하여 frags 배열에 저장
+            // 0, 1, 2, 3 중 랜덤한 인덱스를 뽑아 A, T, C, G 중 하나를 무작위 선택
+            int index = rand() % 4; 
+            frags[i][j] = bases[index]; 
         }
-        frags[i][fragLength] = '\0';      // 문자열 종료 문자 추가
+        frags[i][fragLength] = '\0'; // 문자열 종료 문자 추가
     }
 
     return frags;
 }
 
 int main(void) {
+    srand(time(NULL));
     char* ref = makeRef();        // refLength개의 염기서열을 생성하여 ref에 저장 -> 염기 서열의 길이 변경 가능
 
     printf("<Reference Sequence>\n");
     printf("%s\n\n", ref);
 
-    char** frags = makeFrag(ref);     // ref에서 fragNum개의 조각을 fragLength 길이로 만들어서 frags에 저장
+    char** frags = makeFrag();     // ref에서 fragNum개의 조각을 fragLength 길이로 만들어서 frags에 저장
 
     for (int i = 0; i < fragNum; i++) {     // 조각들이 잘 저장되었는지 확인용.. 나중에 지워도 됨
         printf("%s\n", frags[i]);
